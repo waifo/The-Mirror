c329cb99e13350d7b2449d8e08fbeddc65104640
Mukesh Kumar <mukeshkingk@gmail.com>
2020-05-10 03:50:00 +0530

    adding frontend build code

---
+++ public/precache-manifest.7e979aafc594f7a8f4e911efa5a11e8f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406be232817778194848d50c240ba6f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe3f5372587d26463120",
+    "url": "/static/js/2.41cd0dc5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.41cd0dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f76e8b287681cf0c3c",
+    "url": "/static/js/main.3507f567.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "38efbb4ef6ebb38266d32b09700a9265",
+    "url": "/static/media/MC-1.38efbb4e.jpg"
+  },
+  {
+    "revision": "c28f17958b1963a6904666344104a4f8",
+    "url": "/static/media/MC-2.c28f1795.jpg"
+  },
+  {
+    "revision": "2de756ff226f1e9c4a1480d49ddf8148",
+    "url": "/static/media/MC-3.2de756ff.jpg"
+  },
+  {
+    "revision": "77473b68ed393d3a142efd2c343dbe93",
+    "url": "/static/media/MC-4.77473b68.jpg"
+  },
+  {
+    "revision": "f291df8df03282f2c26a9d93e748540e",
+    "url": "/static/media/TitilliumWeb-ExtraLight.f291df8d.ttf"
+  },
+  {
+    "revision": "edc21ba4e5eb0d77c371a8f0851ed72f",
+    "url": "/static/media/TitilliumWeb-Light.edc21ba4.ttf"
+  },
+  {
+    "revision": "ed1d014bf2b8b72f27d6af65c69f710a",
+    "url": "/static/media/TitilliumWeb-Regular.ed1d014b.ttf"
+  }
+]);
