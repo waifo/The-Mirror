1087bdf7963da1eead326e16afd80972ec907b4f
Mukesh Kumar <mukeshkingk@gmail.com>
2020-05-14 06:14:27 +0530

    Responsive footer from frontend

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.144762eabbabf12d20289c49d7e7c2af.js"
+  "/precache-manifest.b1b72623fc789ca48e49d2f6605d42dc.js"
 );
 
 self.addEventListener('message', (event) => {
