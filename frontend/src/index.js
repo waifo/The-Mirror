1c953a4ca9a327285d1c466b8261abf9b10d73df
waifo <mukeshkingk@gmail.com>
2020-05-17 02:47:46 +0530

    Changing logo and favicon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/browser";
 
 import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 if (process.env === "production") {
   Sentry.init({
@@ -21,4 +21,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.register();
